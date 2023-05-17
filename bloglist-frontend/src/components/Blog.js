bff8b87c528138e84a5bdd9dcc9ea988211396c6
Smiboi <128560300+Smiboi@users.noreply.github.com>
2023-05-17 21:39:32 +0300

    Tehtävät 5.6-5.7

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,23 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [viewInfo, toggleInfo] = useState(false)
+  const label = viewInfo
+    ? 'hide'
+    : 'view'
+  return (
+    <div className="blog">
+      {blog.title} by {blog.author}
+      <button onClick={() => toggleInfo(!viewInfo)}>{label}</button>
+      <div style={{display: viewInfo ? '' : 'none'}}>
+        <div>{blog.url}</div>
+        <div>
+          likes: {blog.likes}
+          <button>like</button>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
