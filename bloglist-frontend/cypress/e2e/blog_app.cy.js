1b02fe3a776231fd922783e8d283f9d3e44fb836
Smiboi <128560300+Smiboi@users.noreply.github.com>
2023-05-20 19:52:00 +0300

    Tehtävä 5.18

--- bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,12 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    // const user = {
-    //   name: 'Matti Meikäläinen',
-    //   username: 'masa',
-    //   password: 'meikä'
-    // }
-    // cy.request('POST', 'http://localhost:3003/api/users/', user)
+    const user = {
+      name: 'Matti Meikäläinen',
+      username: 'masa',
+      password: 'meikä'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -15,5 +15,21 @@ describe('Blog app', function() {
     cy.contains('login to application')
     cy.contains('username')
     cy.contains('password')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('masa')
+      cy.get('#password').type('meikä')
+      cy.get('#login-button').click()
+      cy.contains('Matti Meikäläinen logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('masa')
+      cy.get('#password').type('meikäläinen')
+      cy.get('#login-button').click()
+      cy.contains('wrong credentials')
+    })
   })
 })
